eaa10f7cd735562ae68a39ed76ea78fca3a24a76
Leathrall <114615751+Leathrall@users.noreply.github.com>
2023-11-08 23:06:35 -0500

    Made Camera Changes

--- TimeGame/Assets/Scripts/CameraMove.cs
+++ TimeGame/Assets/Scripts/CameraMove.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour {
 
-    public Transform player;
+    [SerializeField]private Transform target;
+    private Vector3 offset = new Vector3(0f, 1f, -10f);
+    private float smoothTime = 0.25f;
+    private Vector3 velocity = Vector3.zero;
 
     // Update is called once per frame
     void Update () {
-        transform.position = player.transform.position + new Vector3(0, 1, -5);
+        Vector3 targetPosition = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
 }
