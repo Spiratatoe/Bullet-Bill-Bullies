a22c55e5d1149c3ceb5cb695e9033d70c4222fec
guidote <gabrielle.guidote@gmail.com>
2023-11-23 18:35:50 -0500

    basic pause menu

---
+++ TimeGame/Assets/Scripts/Other/PauseMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject PausePanel;
+    [SerializeField] private GameObject UIPanel;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Time.timeScale = 0;
+            PausePanel.SetActive(true);
+            UIPanel.SetActive(false);
+        }    
+    }
+
+    public void Resume() {
+        Time.timeScale = 1;
+        PausePanel.SetActive(false);
+        UIPanel.SetActive(true);
+    }
+
+    public void Exit() {
+        Application.Quit();
+    }
+}
