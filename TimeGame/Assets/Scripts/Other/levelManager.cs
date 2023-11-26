2588cbaa4a26421052bce235a13c50e2b3b388d7
Koosa924 <afaf.anzarouti@gmail.com>
2023-11-25 21:48:44 -0500

    fixed asteroid spawn rate + position

--- TimeGame/Assets/Scripts/Other/levelManager.cs
+++ TimeGame/Assets/Scripts/Other/levelManager.cs
@@ -1,3 +1,6 @@
+
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,22 +8,44 @@ using UnityEngine;
 public class levelManager : MonoBehaviour
 {
     public List<GameObject> asteroidPrefab;
-    public float spawnTime = 0.5f;
+    public float spawnInterval = 0.2f;
+    private Camera mainCamera;
 
     void Start()
     {
-        StartCoroutine(spawnAsteroids());
+        mainCamera = Camera.main;
+        StartCoroutine(SpawnAsteroids());
     }
 
-    IEnumerator spawnAsteroids()
+    IEnumerator SpawnAsteroids()
     {
         while (true)
         {
-            int randomInteger = Random.Range(0, asteroidPrefab.Count);
-            Instantiate(asteroidPrefab[randomInteger], new Vector3(Random.Range(-25f, 50f), 6f, 0f), Quaternion.identity);
-            randomInteger = Random.Range(0, asteroidPrefab.Count);
-            Instantiate(asteroidPrefab[randomInteger], new Vector3(Random.Range(-25f, 50f), 6f, 0f), Quaternion.identity);
-            yield return new WaitForSeconds(spawnTime);
+            SpawnAsteroid();
+            yield return new WaitForSeconds(spawnInterval);
         }
+    }
+
+    void SpawnAsteroid()
+    {
+        // random integer for choosing asteroid prefab
+        int randomInteger = Random.Range(0, asteroidPrefab.Count);
+
+
+        // camera viewport
+
+        float cameraHeight = 2f * mainCamera.orthographicSize;
+        float cameraWidth = cameraHeight * mainCamera.aspect;
+
+        // get random coordinates for viewport
+        float randomViewportX = Random.Range(0f, 1f);
+        float randomViewportY = Random.Range(0f, 1f);
+
+        // change from viewport to world coordinates
+        Vector3 spawnPositionViewport = new Vector3(randomViewportX, 1f, mainCamera.nearClipPlane);
+        Vector3 spawnPositionWorld = mainCamera.ViewportToWorldPoint(spawnPositionViewport);
+
+        // spawn asteroids
+        Instantiate(asteroidPrefab[randomInteger], spawnPositionWorld, Quaternion.identity);
     }
 }
