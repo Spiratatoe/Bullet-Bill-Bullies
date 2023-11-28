ccaf02cd8e5d9e529248ff05dcee2f5fab405c88
guidote <gabrielle.guidote@gmail.com>
2023-11-28 16:55:49 -0500

    added knight hp

--- TimeGame/Assets/Scripts/EnemyHP.cs
+++ TimeGame/Assets/Scripts/EnemyHP.cs
@@ -2,21 +2,46 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHP : MonoBehaviour
 {
     [SerializeField] float hp, maxHP = 3;
     private Animator mAnimator;
+
     [Header ("XP Components")]
     [SerializeField] private float XPValue = 10;
-    public GameObject bottlePrefab;
 
+    public GameObject bottlePrefab;
     public Boolean isInvincible = false;
-    // Start is called before the first frame update
+    
+    [Header ("HP Bar Components")]
+    [SerializeField] private Image bar_fill;
+    public Color32 high_hp_colour;
+    public Color32 half_hp_colour;
+    public Color32 low_hp_colour;
+
     void Start()
     {
         mAnimator = GetComponent<Animator>();
         hp = maxHP;
+
+        // hp bar stuff
+        high_hp_colour = new Color32(157,255,140,255); // RGBA
+        half_hp_colour = new Color32(255,255,140,255); // RGBA
+        low_hp_colour = new Color32(255,140,140,255); // RGBA
+
+        float fillAmount = hp / maxHP;
+        bar_fill.fillAmount = fillAmount;
+        bar_fill.color = high_hp_colour;
+
+        // set camera for the hp bar
+        Camera mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        GameObject.Find("HPBarCanvas").GetComponent<Canvas>().worldCamera = mainCamera;
+    }
+
+    private void Update() {
+        UpdateHealthBar();
     }
 
     public void TakeDamage(float damage)
@@ -66,11 +91,24 @@ public class EnemyHP : MonoBehaviour
             yield return new WaitForSeconds(1f);
 
             //create an XP bottle
-            // GameObject newBottle = Instantiate(bottlePrefab, transform.parent.parent);
-            // newBottle.transform.position = new Vector2 (transform.position.x, transform.position.y - 0.2f);
-            // newBottle.GetComponent<XPBottle>().value = XPValue; 
-            // Destroy(gameObject.transform.parent.gameObject);
+            GameObject newBottle = Instantiate(bottlePrefab, transform.parent.parent);
+            newBottle.transform.position = new Vector2 (transform.position.x, transform.position.y - 0.2f);
+            newBottle.GetComponent<XPBottle>().value = XPValue; 
+            Destroy(gameObject.transform.parent.gameObject);
         }
+    }
+
+    private void UpdateHealthBar()
+    {
+        float fillAmount = hp / maxHP;
+        bar_fill.fillAmount = fillAmount;
+
+        Color32 healthColor;
+
+        if (fillAmount >= 0.7f) { healthColor = high_hp_colour; }
+        else if (fillAmount < 0.7f && fillAmount >= 0.4f) { healthColor = half_hp_colour; }
+        else { healthColor = low_hp_colour; }
+        bar_fill.color = healthColor;
     }
 
 }
