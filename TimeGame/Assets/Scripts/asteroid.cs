d2756b13dddd260671dc361aa55a4ac0977282e2
Leathrall <114615751+Leathrall@users.noreply.github.com>
2023-11-29 04:02:42 -0500

    added timestop to meteors and particles

--- TimeGame/Assets/Scripts/asteroid.cs
+++ TimeGame/Assets/Scripts/asteroid.cs
@@ -6,17 +6,42 @@ public class asteroid : MonoBehaviour
 {
     private float speed = 0.001f;
 
+    //objects to stop during timestop
+    private Rigidbody2D rb;
+    private ParticleSystem ps;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //get components
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        ps = gameObject.GetComponentInChildren<ParticleSystem>();
+        ps.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        //timestop constraints
+        if (TimeStop.timeStopped)
+        {
+            rb.constraints = RigidbodyConstraints2D.FreezeAll;
+            ps.Pause();
+            return;
+        }
+        else
+        { 
+            rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+            if (!ps.isPlaying)
+            {
+                ps.Play();
+            }
+        }
+        
         transform.position += -transform.up * speed;
+      
+        
 
         // destroy asteroid if below equal or below -2 in the y position
         if (transform.position.y <= -2)
