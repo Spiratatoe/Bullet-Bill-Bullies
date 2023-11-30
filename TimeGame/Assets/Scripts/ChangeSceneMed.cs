ffb198a207dc24c4b42bdebaa4c3fe0966a0b64c
jack <jspiratos26@gmail.com>
2023-11-29 22:35:20 -0500

    Portal prefab. + working boss door + key hover
    also some sprites were added

--- TimeGame/Assets/Scripts/ChangeSceneMed.cs
+++ TimeGame/Assets/Scripts/ChangeSceneMed.cs
@@ -4,11 +4,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+
 public class ChangeSceneMed : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D other)
+
+    public GameObject canvas;
+    private void OnTriggerStay2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
+        {
+            canvas.SetActive(true);
+        }
+        if (other.gameObject.CompareTag("Player") && Input.GetKey(KeyCode.Q))
         {
             Debug.Log("does this collide");
             SceneManager.LoadScene("MedievalBoss");
@@ -16,5 +24,9 @@ public class ChangeSceneMed : MonoBehaviour
         
         
     }
-    
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        canvas.SetActive(false);
+    }
 }
